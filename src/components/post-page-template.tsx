02f0f6f32d2057131b33c700d1a125aa3a2bd23a
Neil Italia <neilitalia1998@gmail.com>
2021-12-24 11:20:57 -0600

    :recycle: refactor post page with ts interface

--- src/components/post-page-template.tsx
+++ src/components/post-page-template.tsx
@@ -2,19 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
-export const query = graphql`
-  query PostsByID($id: String!) {
-    mdx(id: { eq: $id }) {
-      body
-      frontmatter {
-        title
-        date(formatString: "dddd, MMMM DD, YYYY", locale: "en-US")
-      }
-    }
-  }
-`
-
-const PostPage = ({ data }) => {
+const PostPage: React.FC<Props> = ({ data }) => {
   console.log(data)
   const { frontmatter, body } = data.mdx
   return (
@@ -27,3 +15,46 @@ const PostPage = ({ data }) => {
 }
 
 export default PostPage
+
+export const query = graphql`
+  query PostsByID($id: String!) {
+    mdx(id: { eq: $id }) {
+      body
+      frontmatter {
+        title
+        date(formatString: "dddd, MMMM DD, YYYY", locale: "en-US")
+      }
+    }
+  }
+`
+
+interface Props {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        description: string
+      }
+    },
+    allMdx: {
+      nodes: [{
+        excerpt: string,
+        frontmatter: {
+          date: string
+          title: string
+        },
+        fields: {
+          slug: string
+        }
+      }]
+    },
+    mdx: {
+      body: string,
+      frontmatter: {
+        title: string,
+        date: string
+      }
+    }
+  }
+}
+
