1cdcd8d35ba277b9dbd5ef3d7546a0e8eeb9c850
Neil Italia <neilitalia1998@gmail.com>
2022-01-21 18:08:08 -0600

    :bug: fix featuredImage not showing without alt bug

--- src/components/post-page-template.tsx
+++ src/components/post-page-template.tsx
@@ -6,10 +6,14 @@ import Layout from "./Layout"
 import PostTags from "./PostTags"
 
 const PostPage: React.FC<PostPageTemplateProps> = ({ data }) => {
+
   const { frontmatter, body, fields } = data.mdx
+  const src: ( string | boolean ) = frontmatter.featuredImage?.childImageSharp.fluid.src || false
+  const alt: string = frontmatter.alt || "Placeholder Alt"
+
   return (
     <Layout>
-      {frontmatter.alt && frontmatter.featuredImage?.childImageSharp.fluid.src && <img src={frontmatter.featuredImage?.childImageSharp.fluid.src} alt={frontmatter.alt} className="w-full rounded-lg mb-7"/>}
+      {src && <img src={src} alt={alt} className="w-full rounded-lg mb-7"/>}
       <article className="prose prose-invert prose-amber prose-headings:text-amber-400 prose-p:text-amber-100 prose-p:font-serif prose-a:text-amber-400 prose-code:text-teal-400 prose-pre:text-teal-400 hover:prose-a:text-amber-600 prose-li:font-serif marker:text-amber-400 marker:opacity-0">
         <h1 className="mb-0 text-6xl">{frontmatter.title}</h1>
         <div className="flex flex-row">
@@ -40,7 +44,7 @@ export const query = graphql`
         alt
         featuredImage {
           childImageSharp {
-            fluid(jpegQuality: 95) {
+            fluid(jpegQuality: 90) {
               src
             }
           }
